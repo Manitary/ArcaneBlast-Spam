c0aa132ac60e1e06f1e2032a441728a901ec5fd7
Manitary <pak.ska@gmail.com>
2021-12-05 20:24:02 +0000

    Remove deprecated files. Update Readme with a to do list.

--- ArcaneBlast_Spam.lua
+++ ArcaneBlast_Spam.lua
@@ -1,12 +1,55 @@
 local AB = { }
-local frame = CreateFrame('Frame')
+local frame = CreateFrame('Button', _, UIParent)
+--frame position
+frame:SetWidth(100)
+frame:SetHeight(100)
+frame:SetPoint('CENTER', UIParent, 'CENTER')
+
+--frame position saved between sessions
+--this breaks if the game is loaded without the addon, then again with it
+--needs to be reworked into a per character saved variable
+frame:SetMovable(true)
+frame:SetUserPlaced(true)
+frame:EnableMouse(false)
+frame:SetScript('OnDragStart', frame.StartMoving)
+frame:SetScript('OnDragStop', frame.StopMovingOrSizing)
+
+--frame text setup
+frame.text = frame:CreateFontString('frame_text', 'OVERLAY', 'GameFontNormalLarge')
+frame.text:SetPoint('CENTER', 0, -10)
+frame.text:SetText('Test')
+
+--extra text setup
+frame.stats = frame:CreateFontString('frame_stats', 'OVERLAY', 'GameFontNormalLarge')
+frame.stats:SetPoint('TOP', 0, 0)
+frame.stats:SetText('Test stats')
+
+--extra text setup
+frame.buffs = frame:CreateFontString('frame_stats', 'OVERLAY', 'GameFontNormalLarge')
+frame.buffs:SetPoint('RIGHT', 100, 200)
+frame.buffs:SetText('Test buffs')
+
+--frame background
+frame.bg = frame:CreateTexture('frame_bg', 'BACKGROUND')
+frame.bg:SetPoint('CENTER',0,0)
+frame.bg:SetWidth(100)
+frame.bg:SetHeight(100)
+frame.bg:SetTexture('Interface\\DialogFrame\\UI-DialogBox-Background')
+--frame.bg:SetTexture(0.0, 0.0, 1.0, 0.5)
+
+--display the frame
+--frame:Show()
+frame:Hide()
 
 frame:RegisterEvent('PLAYER_LOGIN')
+--frame:RegisterEvent('PLAYER_LOGOUT')
+--frame:RegisterEvent('ADDON_LOADED')
 
 --get AB ready to check the player on talent change
 function AB:StartUp()
 	frame:RegisterEvent('CHARACTER_POINTS_CHANGED')
 	frame:RegisterEvent('ACTIVE_TALENT_GROUP_CHANGED')
+	frame:RegisterEvent('PLAYER_EQUIPMENT_CHANGED')
 --NOTE: On a fresh login, after PLAYER_LOGIN the game still does not know talents, PLAYER_ALIVE is needed for that
 	frame:RegisterEvent('PLAYER_ALIVE')
 end
@@ -21,21 +64,21 @@ local buffList = {
 	ArcaneFocus = {
 		--talent 1,2
 		name = 'Arcane Focus',
-		active = false,
+		active = true,
 		type = 'talent',
 		--cost = -1/2/3,
 	},
 	NetherwindPresence = {
 		--talent 1,28
 		name = 'Netherwind Presence',
-		active = false,
+		active = true,
 		type = 'talent',
 		--haste = 2/4/6,
 	},
 	Precision = {
 		--talent 3,6
 		name = 'Precision',
-		active = false,
+		active = true,
 		type = 'talent',
 		--cost = -1/2/3
 	},
@@ -100,6 +143,15 @@ local buffList = {
 		maxDuration = 10,
 		maxCooldown = 180,
 	},
+	EngiGloves = {
+		name = 'Hyperspeed Accelerators',
+		active = false,
+		type = 'equip',
+		rating = 340,
+		maxDuration = 12,
+		maxCooldown = 60,
+	},
+	--]]
 	--[[
 	PotionSpeed = {
 		name = 'Potion of Speed',
@@ -116,14 +168,15 @@ local buffList = {
 		hasterating = 0,
 	},
 	]]--
-	--[[
 	ManaGem = {
 		name = 'Mana Sapphire',
-		active = true,
+		active = false,
 		type = 'item',
-		???
-	}
-	--]]
+		manaFlat = 3330,
+		manaFlatMax = 3500,
+		maxCooldown = 120,
+		charges = 3,
+	},
 	--[[
 	Evocation = {
 		???
@@ -134,8 +187,8 @@ local buffList = {
 local ComputeManaCost = function(cost, stacks, buffs)
 	local x = cost * (1 + 1.75 * stacks)
 	for k, buff in pairs(buffs) do
-		if buff.mana then
-			x = x * (1 + buff.mana / 100)
+		if buff.active and buff.cost then
+			x = x * (1 + buff.cost / 100)
 		end
 	end
 	return x
@@ -144,10 +197,11 @@ end
 local ComputeCastTime = function(cast, buffs)
 	local x = cast
 	for k, buff in pairs(buffs) do
-		if buff.haste then
+		if buff.active and buff.haste then
 			x = x / (1 + buff.haste / 100)
 		end
 	end
+	x = x / (1 + GetCombatRatingBonus(20) / 100)
 	return x
 end
 
@@ -177,10 +231,27 @@ function AB:CheckCharacter()
 		print('Arcane Mage detected')
 		frame:RegisterEvent('PLAYER_REGEN_ENABLED')
 		frame:RegisterEvent('PLAYER_REGEN_DISABLED')
+		--remake this using the talent type maybe?
 		buffList.ArcaneFocus.cost = -select(5, GetTalentInfo(1, 2))
 		buffList.NetherwindPresence.haste = 2 * select(5, GetTalentInfo(1, 28))
 		buffList.Precision.cost = -select(5, GetTalentInfo(3, 6))
-		--remake this using the talent type
+		if not IsSpellKnown(26297) then
+			buffList.Berserking = nil
+		end
+		local gloves = GetInventoryItemLink("player", 10)
+		--unlike racial, gloves enchant can be added/removed on the fly
+		if (not gloves) or select(3, strsplit(":", gloves)) ~= '3604' then
+			buffList.EngiGloves = nil
+		else
+			buffList.EngiGloves = {
+				name = 'Hyperspeed Accelerators',
+				active = false,
+				type = 'equip',
+				rating = 340,
+				maxDuration = 12,
+				maxCooldown = 60,
+			}
+		end
 	end
 end
 
@@ -194,21 +265,173 @@ SlashCmdList["AB"] = function(msg)
 	elseif msg == 'off' then
 		print('AB off')
 		frame:UnregisterAllEvents()
-	else
-		print('Use: /ab on/off')
+		frame:Hide()
+	elseif msg == 'lock' then
+		--frame movable
+		--maybe add a slash command to make it (un)movable
+		if not UnitAffectingCombat('player') then
+			if frame:IsMouseEnabled() then
+				frame:Hide()
+				frame:EnableMouse(false)
+				frame:RegisterForDrag()
+			else
+				frame:Show()
+				frame:EnableMouse(true)
+				frame:RegisterForDrag('LeftButton')
+			end
+		else
+			print('AB cannot be moved while in combat')
+		end
+	else	
+		print('Use: /ab on/off/lock')
 	end
 end
 
---when entering combat, register the events from which the calculation is performed
---also checks for raid buffs: boomkin/retri haste, spell haste totem
+local timeToDie
+
+local timeSinceLastUpdate = 0
+local update_interval = 0.05
+
+local UpdatePrediction = function(self, elapsed)
+	timeSinceLastUpdate = timeSinceLastUpdate + elapsed
+
+	while timeSinceLastUpdate > update_interval do
+
+		local baseManaCost, manaCost = 142.45
+		-- 2035 base mana -> 142.45 base mana cost of AB
+		local baseCastTime, castTime = 2.5
+		local time, stacks, manaCurrent, manaMax, manaRegen, manaGain = 0, 0
+
+		manaCurrent = UnitMana('player')
+		manaMax = UnitManaMax('player')
+		manaRegen = select(2, GetManaRegen())
+		stacks = select(4, UnitDebuff('player', 'Arcane Blast')) or 0
+		buffList.ManaGem.charges = GetItemCount(33312, nil, true)
+
+		--check external buffs and set duration if found
+		--also check charges/cd of mana gem
+		for k, buff in pairs(buffList) do
+			if buff.type == 'temporary' then
+				local name, _, _, _, _, _, expirationTime = UnitBuff('player', buff.name)
+				if name then
+					buff.active = true
+					buff.remainingDuration = expirationTime - GetTime()
+				else
+					buff.active = false
+					buff.remainingDuration = nil
+				end
+				if buff.maxCooldown then
+					local activationTime, spellCooldown = GetSpellCooldown(buff.name)
+					buff.remainingCooldown = activationTime == 0 and 0 or (activationTime + spellCooldown - GetTime())
+				end
+			elseif k == 'ManaGem' then
+				buff.charges = GetItemCount(33312, nil, true)
+				local activationTime, itemCooldown = GetItemCooldown(33312)
+				buff.remainingCooldown = activationTime == 0 and 0 or (activationTime + itemCooldown - GetTime())
+			end
+		end
+
+		--temporary
+		--buff list for troubleshooting
+		local currentbuffs = ''
+		for k, buff in pairs(buffList) do
+			if buff.active == true then
+				currentbuffs = currentbuffs .. buff.name .. '\n'
+			end
+		end
+		frame.buffs:SetText(currentbuffs)
+
+		local spellCastName, _, _, _, _, endCastTime = UnitCastingInfo('player')
+		local spellChannelName, _, _, _, _, endChannelTime = UnitChannelInfo('player')
+		time = spellCastName and endCastTime / 1000 - GetTime() or 0
+		time = spellChannelName and endChannelTime / 1000 - GetTime() or time
+
+		if time > 0 then
+			for k, buff in pairs(buffList) do
+				if buff.type == 'temporary' or buff.type == 'item' then
+					UpdateBuff(buff, time)
+				end
+			end
+			if spellCastName then
+				local tempCost = select(4, GetSpellInfo(spellCastName))
+				manaCurrent = math.min(manaCurrent + manaRegen * time, manaMax) - tempCost
+				if spellCastName == 'Arcane Blast' then
+					stacks = math.min(stacks + 1, 4)
+				end
+			end
+		end
+
+		manaCost = ComputeManaCost(baseManaCost, stacks, buffList)
+		castTime = ComputeCastTime(baseCastTime, buffList)
+		frame.stats:SetText('Cast time: ' .. math.floor(castTime * 100) / 100 .. '\n' .. 'Mana Cost: ' .. math.floor(manaCost * 100) / 100)	
+
+		count = 0
+		--loop
+		while true do
+
+			--if any self-buff is available, activate it and start cooldown (except mana gem/evocation)
+			--need to implement a logic not to overlap haste cooldowns (berserking, icy veins, engi gloves)
+			for k, buff in pairs(buffList) do
+				if type == 'temporary' and buff.remainingCooldown == 0 then
+					buff.active = true
+					buff.remainingDuration = buff.maxDuration
+				end
+			end
+
+			--compute mana cost
+			manaCost = ComputeManaCost(baseManaCost, stacks, buffList)
+			--compute cast time
+			castTime = ComputeCastTime(baseCastTime, buffList)
+			--compute mana regen during cast time
+			manaGain = manaRegen * castTime
+
+			--use mana gem if availale and isn't wasted even with max proc
+			if buffList.ManaGem.charges > 0 and buffList.ManaGem.remainingCooldown == 0 then
+				if manaCurrent + manaGain + buffList.ManaGem.manaFlatMax < manaMax then
+					buffList.ManaGem.charges = buffList.ManaGem.charges - 1
+					manaCurrent = manaCurrent + buffList.ManaGem.manaFlat
+				end
+			end
+
+			--break when you you can't cast anymore
+			if manaCost > manaCurrent then
+				break
+			end
+
+			--update after casting
+			time = time + castTime
+			count = count + 1
+			stacks = math.min(stacks + 1, 4)
+			manaCurrent = math.min(manaCurrent + manaGain, manaMax) - manaCost
+			for k, buff in pairs(buffList) do
+				if buff.type == 'temporary' then
+					UpdateBuff(buff, castTime)
+				end
+			end
+		end
+
+		--display format (s or m:ss)
+		if time < 60 then
+			frame.text:SetText('Time left: ' .. string.format("%.1f", time) .. '\n' .. 'Casts left: ' .. count)
+		else
+			frame.text:SetText('Time left: ' .. string.format("%d:%04.1f", time / 60, time % 60) .. '\n' .. 'Casts left: ' .. count)
+		end
+
+		--change colour depending on a TimeToDie feed, if available
+		if timeToDie and time > timeToDie then
+			frame.text:SetTextColor(0, 1, 0, 1)
+		else
+			frame.text:SetTextColor(1, 0, 0, 1)
+		end
+
+		timeSinceLastUpdate = timeSinceLastUpdate - update_interval
+	end
+end
+
 function AB:PLAYER_REGEN_DISABLED()
 	print('Entering combat')
-	frame:RegisterEvent('UNIT_MANA')
-	frame:RegisterEvent('UNIT_MAXMANA')
-	frame:RegisterEvent('UNIT_AURA')
-	frame:RegisterEvent('UNIT_SPELLCAST_SUCCEEDED')
 	for k, buff in pairs(buffList) do
-		if buff.type == 'temporary' then
+		if buff.type == 'static' then
 			local name, rank = UnitBuff('player', buff.name)
 			if name then
 				buff.active = true
@@ -219,98 +442,33 @@ function AB:PLAYER_REGEN_DISABLED()
 			end
 		end
 	end
+	frame:RegisterEvent('CHAT_MSG_ADDON')
+	frame:SetScript('OnUpdate', UpdatePrediction)
+	frame:Show()
 end
 
---when leaving combat, unregister the same events
 function AB:PLAYER_REGEN_ENABLED()
 	print('Leaving combat')
-	frame:UnregisterEvent('UNIT_MANA')
-	frame:UnregisterEvent('UNIT_MAXMANA')
-	frame:UnregisterEvent('UNIT_AURA')
-	frame:UnregisterEvent('UNIT_SPELLCAST_SUCCEEDED')
+	frame:UnregisterEvent('CHAT_MSG_ADDON')
+	frame:SetScript('OnUpdate', nil);
+	frame:Hide()
+	timeSinceLastUpdate = 0
+	timeToDie = nil
 end
 
-local baseManaCost, manaCost = 142.45
--- 2035 base mana -> 142.45 base mana cost of AB
-local baseCastTime, castTime = 2.5
-local time, stacks, manaCurrent, manaMax, manaRegen, manaGain = 0, 0
+function AB:CHAT_MSG_ADDON(...)
+	local prefix, message, channel, source = ...
+	if prefix == 'Manitary - ABS' and source == UnitName('player') then
+		timeToDie = tonumber(message)
+	end
+end
 
 --general event handler
 frame:SetScript('OnEvent', function(self, event, ...)
 	if AB[event] then 
-		print(event)
 		AB[event](AB, ...)
 	end
-	if event == 'CHARACTER_POINTS_CHANGED' or event == 'ACTIVE_TALENT_GROUP_CHANGED' or event == 'PLAYER_ALIVE' or event == 'PLAYER_LOGIN' then
+	if event == 'CHARACTER_POINTS_CHANGED' or event == 'ACTIVE_TALENT_GROUP_CHANGED' or event == 'PLAYER_ALIVE' or event == 'PLAYER_LOGIN' or event == 'PLAYER_EQUIPMENT_CHANGED' then
 		AB:CheckCharacter()
-	end
-	if event == 'UNIT_MANA' or event == 'UNIT_MAXMANA' or event == 'UNIT_AURA' or event == 'UNIT_SPELLCAST_SUCCEEDED' then
-		local unit = select(1, ...)
-		if unit ~= 'player' then return end
-		print(event)
-		manaCurrent = UnitMana('player')
-		manaMax = UnitManaMax('player')
-		manaRegen = select(2, GetManaRegen())
-		stacks = select(4, UnitDebuff('player', 'Arcane Blast')) or 0
-
-		--check external buffs and set duration if found
-		for k, buff in pairs(buffList) do
-			if buff.type == 'temporary' then
-				local name, _, _, _, _, _,  expirationTime = UnitBuff('player', buff.name)
-				if name then
-					buff.active = true
-					buff.remainingDuration = expirationTime - GetTime()
-					--print(name)
-				else
-					buff.active = false
-					buff.remainingDuration = nil
-				end
-				if buff.maxCooldown then
-					local activationTime, spellCooldown = GetSpellCooldown(buff.name)
-					buff.remainingCooldown = activationTime == 0 and 0 or (activationTime + spellCooldown - GetTime())
-					--print(buff.remainingCooldown)
-				end
-			end
-		end
-
-		time = 0
-		count = 0
-		--loop
-		while true do
-			--if any self-buff is available, activate it and start cooldown (except mana gem/evocation)
-			for k, buff in pairs(buffList) do
-				if type == 'temporary' and buff.remainingCooldown == 0 then
-					buff.active = true
-					buff.remainingDuration = buff.maxDuration
-				end
-			end
-			--compute mana cost
-			manaCost = ComputeManaCost(baseManaCost, stacks, buffList)
-			--compute cast time
-			castTime = ComputeCastTime(baseCastTime, buffList)
-			--compute mana regen during cast time
-			manaGain = manaRegen * castTime
-			--if manaCurrent + manaGain + [manaGem] < manaMax then
-				--use managem
-				--manaCurrent = manaCurrent + manaGem
-			--end
-			--break when you you can't cast anymore
-			if manaCost > manaCurrent then
-				break
-			end
-			--update after casting
-			time = time + castTime
-			count = count + 1
-			manaCurrent = math.min(manaCurrent + manaGain, manaMax) - manaCost
-			for k, buff in pairs(buffList) do
-				if buff.type == 'temporary' then
-					UpdateBuff(buff, castTime)
-				end
-			end
-		end
-		--print('Time left:' .. math.floor(time * 100) / 100 )
-		--print('Cast left:' .. count)
-		--placeholder until creating a visual frame
-		SendAddonMessage('AB', math.floor(time * 100) / 100 .. " - " .. count, 'WHISPER', UnitName('player'))
 	end
 end)
--- README.md
+++ README.md
@@ -4,3 +4,15 @@ World of Warcraft 3.3.5 AddOn to optimize Arcane Mage DPS
 The AddOn simulates a spam of Arcane Blast - i.e. no cast of Arcane Missiles, including use of cooldowns, and gives a time estimate on how long you can keep up without running out of mana.
 	
 This is useful in combination with a TimeToDie AddOn/WeakAura to know when you can stop casting Arcane Missiles, in order to optimise your mana usage near the end of a boss fight.
+
+----
+
+### Version 0.2 To do list
+
+* Implement haste rating bonuses from gear/items: Potion of Speed, Hyperspeed Accelerators, Trinkets (anything else?)
+
+* Reset texts on combat end
+
+* Use SavedVariables to store frame position, instead of relying on SetUserPlaced
+
+* Add customisation options (e.g. background/text colour, font type and size)
